d2e765b9a7f1552bd25a06e009a861c565aea8da
David Baumwald <david.baumwald@gmail.com>
2019-07-02 19:42:37 -0400

    Latest Updates

--- index.js
+++ index.js
@@ -1,77 +1,107 @@
-import './main.scss'
+import './main.scss';
 
-;( function(){
-    'use strict'
+class GDQDonations {
+    constructor() {
+        this.container = document.querySelector( '.gdq-donations > div' );
+        this.donationSeed = 10000; // Milliseconds
+        this.possibleDonations = [
+            5000,
+            2500,
+            1000,
+            1000,
+            ...Array( 3 ).fill().map( () => 500 ),
+            ...Array( 6 ).fill().map( () => 250 ),
+            ...Array( 8 ).fill().map( () => 100 ),
+            ...Array( 15 ).fill().map( () => 75 ),
+            ...Array( 30 ).fill().map( () => 50 ),
+            ...Array( 90 ).fill().map( () => 25 ),
+            ...Array( 200 ).fill().map( () => 10 ),
+        ];
 
-    const main = document.querySelector( '.main' );
-    const container = main.querySelector( '.container' );
-    const total = container.querySelector( '.total' );
+        this.index = 0;
 
-    const containerPosition = container.getBoundingClientRect();
-    const minX = containerPosition.left;
-    const maxX = containerPosition.right;
-    const minY = containerPosition.top + 100;
-    const maxY = containerPosition.bottom - 100;
+        this.init();
+    }
 
-    let currentTotal =  Number( total.dataset.total );
+    init() {
+        return new Promise( ( resolve, reject ) => {
+            if ( null === this.container ) {
+                reject( 'Container does not exist!' );
+            }
 
-    console.log( 'containerPosition', containerPosition );
-    console.log( 'minX', minX );
-    console.log( 'maxX', maxX );
-    console.log( 'minY', minY );
-    console.log( 'maxY', maxY );
+            this.total        = this.container.querySelector( '.total' );
+            this.currentTotal =  Number( this.total.dataset.total );
 
-    function addDonation( value ) {
+            this.containerPosition = this.container.getBoundingClientRect();
+            this.containerTop      = this.containerPosition.top;
+            this.containerBottom   = this.containerPosition.bottom;
+            this.containerLeft     = this.containerPosition.left;
+            this.containerRight    = this.containerPosition.right;
+            this.containerHeight   = this.containerPosition.height;
+            this.containerWidth    = this.containerPosition.width;
+
+            this.minTop    = this.containerTop + 40;
+            this.minBottom = this.containerTop + this.containerHeight - 30;
+
+            resolve();
+        } ).then( () => {
+            this.loop();
+        } ).catch( e => {
+            console.warn( 'GDQ Donations', 'Container does not exist in document!' );
+        } );
+    }
+
+    addDonation( value ) {
         const amountToAdd = Number( value );
         const donation    = document.createElement( 'div' );
-        const amount      = document.createTextNode( `$${amountToAdd}` );
+        const amount      = document.createTextNode( `$${ amountToAdd.toString().replace( /\B(?=(\d{3})+(?!\d))/g, "," ) }` );
 
-        const top  = Math.floor( ( Math.random() * ( maxY - minY ) ) + minY / 2 );
-        const left = Math.floor( ( Math.random() * ( maxX - minX ) ) );
+        const top  = Math.floor( Math.random() * ( this.minBottom - this.minTop ) );
+        const left = Math.floor( Math.random() * ( this.containerRight - this.containerLeft ) );
 
         donation.classList.add( 'donation' );
         donation.style.top  = `${top}px`;
         donation.style.left = `${left}px`;
 
         donation.appendChild( amount );
-        container.appendChild( donation );
+        this.container.appendChild( donation );
 
         donation.classList.add( 'added' );
 
-        let newTotal = currentTotal + amountToAdd;
+        if ( amountToAdd >= 20 ) {
+            donation.classList.add( 'major' );
+        }
+
+        let newTotal = this.currentTotal + amountToAdd;
 
         let interval = setInterval( () => {
-            total.textContent = `$${currentTotal.toString().replace( /\B(?=(\d{3})+(?!\d))/g, "," )}`;
+            this.total.textContent = `$${ this.currentTotal.toString().replace( /\B(?=(\d{3})+(?!\d))/g, "," ) }`;
 
-            if ( currentTotal >= newTotal ) {
+            if ( this.currentTotal >= newTotal ) {
                 clearInterval( interval );
             }
 
-            currentTotal++;
+            ( newTotal - this.currentTotal ) > 500 ? this.currentTotal += ( amountToAdd - 500 ) : this.currentTotal++;
         }, Math.floor( ( 1 / amountToAdd ) * 1000 ) );
 
         setTimeout( () => {
-            // donation.parentNode.removeChild( donation );
+            donation.parentNode.removeChild( donation );
         }, 5000 );
     }
 
-    const possibleDonations = [
-        1000,
-        500,
-        ...Array( 3 ).fill().map( () => 250 ),
-        ...Array( 8 ).fill().map( () => 100 ),
-        ...Array( 15 ).fill().map( () => 75 ),
-        ...Array( 30 ).fill().map( () => 50 ),
-        ...Array( 90 ).fill().map( () => 25 ),
-        ...Array( 200 ).fill().map( () => 10 ),
-    ];
+    loop() {
+        this.addDonation( this.possibleDonations[ Math.floor( Math.random() * this.possibleDonations.length ) ] );
+        this.index++;
 
-    ( function donationLoop() {
-        let interval = Math.round( Math.random() * 5000 ) + 2000;
+        clearInterval( this.donationInterval );
 
-        setTimeout( () => {
-            addDonation( possibleDonations[ Math.floor( Math.random() * possibleDonations.length ) ] );
-            donationLoop();
-        }, interval );
-    }() );
+        // if ( this.index >= 8 ) return;
+        let interval = Math.round( Math.random() * this.donationSeed );
+
+        this.donationInterval = setInterval( () => this.loop(), interval );
+    }
+}
+
+;( function() {
+    new GDQDonations();
 } )();
